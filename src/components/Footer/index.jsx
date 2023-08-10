f19b3cedbff7779ad4156df603a4e295ebe397d6
AngelCstd <luisacast.29@gmail.com>
2023-08-10 17:44:52 -0600

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,15 @@
 import "./Footer.css"
 
-export function Footer(){
-    return(
-        <footer>
+export function Footer() {
+    return (
+        <footer className="footer" >
             <ul className="footer__ul">
                 <li className="footer__item">
-                    <p className="footer__img">instagram</p>
+                    <a href="https://www.instagram.com/_rakuun_/" target="_blank"><img src="./img/instagram.svg" alt="" /></a>
                 </li>
                 <li className="footer__item">
-                    <p className="footer__img">Tiktok</p>
-                </li>
-                <li className="footer__item">
-                    <p className="footer__img">Whatsapp</p>
+                    <a href="https://www.tiktok.com/@eli_cstcc" target="_blank"><img src="./img/tiktok.svg" alt="" /></a>
+
                 </li>
             </ul>
         </footer>
