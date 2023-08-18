3248df8b57eb4e35598ed86d1c168f3ab8aeb8f3
AngelCstd <luisacast.29@gmail.com>
2023-08-18 15:27:24 -0600

    Listo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,25 @@ import { Footer } from "./components/Footer";
 import { Inicio } from "./components/Inicio";
 import { Carga } from "./components/Carga";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [dataHeader, setDataHeader] = useState({})
+
+useEffect(()=>{
+ async function fetchDataHeader() {
+  try {
+    const data = await fetch("https://obtencion-imagenes-back.vercel.app/notion")
+    setDataHeader(data)
+  } catch (error) {
+    console.log(error)
+  }
+ }
+ fetchDataHeader()
+ console.log(dataHeader)
+},[])
+
   return (
     <BrowserRouter>
       <HeaderNav />
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,15 @@
 import "./Footer.css"
+import { Tiktok, Instagram } from "../Icons";
 
 export function Footer() {
     return (
         <footer className="footer" >
             <ul className="footer__ul">
                 <li className="footer__item">
-                    <a href="https://www.instagram.com/_rakuun_/" target="_blank"><img src="./img/instagram.svg" alt="" /></a>
+                    <a href="https://www.instagram.com/_rakuun_/" target="_blank"><Instagram/> </a>
                 </li>
                 <li className="footer__item">
-                    <a href="https://www.tiktok.com/@eli_cstcc" target="_blank"><img src="./img/tiktok.svg" alt="" /></a>
+                    <a href="https://www.tiktok.com/@eli_cstcc" target="_blank"><Tiktok/> </a>
 
                 </li>
             </ul>
